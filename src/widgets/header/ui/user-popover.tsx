c60b5d1bcda351700c99b87c89c43da658ccce45
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2025-01-05 01:21:28 +0700

    feat(user-popover): add ghost buttons

--- src/widgets/header/ui/user-popover.tsx
+++ src/widgets/header/ui/user-popover.tsx
@@ -1,3 +1,4 @@
+import { getUserCompanyQueryOptions } from "@/entities/company";
 import { Routes } from "@/shared/constants/routes";
 import { useSession } from "@/shared/store/session";
 import { Button } from "@/shared/ui/button";
@@ -5,10 +6,14 @@ import { Icons } from "@/shared/ui/icons";
 import { Popover, PopoverContent, PopoverTrigger } from "@/shared/ui/popover";
 import { Separator } from "@/shared/ui/separator";
 import { TextMorph } from "@/shared/ui/text-morph";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
 export function UserPopover() {
   const user = useSession((state) => state.user);
+  const { data: company, isLoading: isCompanyLoading } = useQuery(
+    getUserCompanyQueryOptions()
+  );
 
   return (
     <Popover>
@@ -21,32 +26,58 @@ export function UserPopover() {
         </Button>
       </PopoverTrigger>
       <PopoverContent side="bottom" align="end" className="pb-3">
-        <h3 className="text-h3">NABUCCO Architecture & Construction</h3>
+        <TextMorph as="h3" className="text-h3">
+          {!isCompanyLoading && company ? company.title : "Загрузка..."}
+        </TextMorph>
+        {/* {!isCompanyLoading && company ? (
+          <h3 className="text-h3">{company.title}</h3>
+        ) : (
+          <Skeleton className="rounded-md h-6 w-32" />
+        )} */}
         <nav className="mt-2">
-          <a
-            href="https://astronbuildings.com/"
-            className="flex items-center gap-2 py-2"
-            target="_blank">
-            <span>Основной сайт</span>
-            <Icons.ExternalLink className="text-border" />
-          </a>
+          <Button
+            variant="ghost"
+            asChild
+            size="sm"
+            className="font-normal w-full justify-start">
+            <a href="https://astronbuildings.com/" target="_blank">
+              <span>Основной сайт</span>
+              <Icons.ExternalLink className="text-border" />
+            </a>
+          </Button>
           <Separator className="my-2" />
-          <Link
-            to={`${Routes.Favorites}?filter=projects`}
-            className="flex justify-between gap-2 items-center pt-2 pb-[0.375rem]">
-            <span>Избранные проекты</span>
-            <span className="text-muted">12</span>
-          </Link>
-          <Link
-            to={`${Routes.Favorites}?filter=docs`}
-            className="flex justify-between gap-2 items-center pt-[0.375rem] pb-2">
-            <span>Избранные документы</span>
-            <span className="text-muted">8</span>
-          </Link>
+          <Button
+            asChild
+            variant="ghost"
+            size="sm"
+            className="w-full justify-start font-normal">
+            <Link
+              to={`${Routes.Favorites}?filter=projects`}
+              className="justify-between">
+              <span>Избранные проекты</span>
+              <span className="text-muted">12</span>
+            </Link>
+          </Button>
+          <Button
+            asChild
+            variant="ghost"
+            size="sm"
+            className="w-full justify-start font-normal">
+            <Link
+              to={`${Routes.Favorites}?filter=docs`}
+              className="justify-between">
+              <span>Избранные документы</span>
+              <span className="text-muted">8</span>
+            </Link>
+          </Button>
           <Separator className="my-2" />
-          <Link to={Routes.Profile} className="py-2 flex">
-            Мой профиль
-          </Link>
+          <Button
+            asChild
+            variant="ghost"
+            size="sm"
+            className="w-full justify-start font-normal">
+            <Link to={Routes.Profile}>Мой профиль</Link>
+          </Button>
         </nav>
       </PopoverContent>
     </Popover>
