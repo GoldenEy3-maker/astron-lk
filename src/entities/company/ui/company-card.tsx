f20c0af00f54704dc42c200f36b88972f4194bb4
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2025-01-06 22:01:23 +0700

    feat(company-card): add skeleton loading

--- src/entities/company/ui/company-card.tsx
+++ src/entities/company/ui/company-card.tsx
@@ -4,63 +4,110 @@ import { Icons } from "@/shared/ui/icons";
 import { Button } from "@/shared/ui/button";
 import { getProjectsCountText } from "../lib/get-projects-count-text";
 import { getCooperationYearsText } from "../lib/get-cooperation-years-text";
+import { Skeleton } from "@/shared/ui/skeleton";
 
 export function CompanyCard() {
-  const { data } = useQuery(getUserCompanyQueryOptions());
+  const { data, isLoading } = useQuery(getUserCompanyQueryOptions());
 
   return (
     <article className="bg-card rounded-main items-start flex gap-14 py-9 px-14 border-b-2 border-gold">
-      <img src={data?.logo} alt={data?.title} />
+      {!isLoading ? (
+        <img src={data?.logo} alt={data?.title} className="w-[8rem] h-24" />
+      ) : (
+        <Skeleton className="w-[8rem] h-24 !rounded-main" />
+      )}
       <div className="flex-1">
         <div className="flex justify-between items-start gap-6">
-          <h2 className="text-h2 text-heading-h3 max-w-[31.25rem]">
-            {data?.title}
-          </h2>
-          <div className="flex items-center gap-3 text-gold font-medium">
-            <Icons.Cup />
-            <span>Золотой Партнёр</span>
-          </div>
+          {!isLoading ? (
+            <h2 className="text-h2 text-heading-h3 max-w-[31.25rem]">
+              {data?.title}
+            </h2>
+          ) : (
+            <div className="space-y-2 w-full">
+              <Skeleton className="w-3/5 h-4 !rounded-full" />
+              <Skeleton className="w-2/5 h-4 !rounded-full" />
+            </div>
+          )}
+          {!isLoading ? (
+            <div className="flex items-center gap-3 text-gold font-medium">
+              <Icons.Cup />
+              <span>Золотой Партнёр</span>
+            </div>
+          ) : (
+            <Skeleton className="w-1/4 h-5 !rounded-full" />
+          )}
         </div>
         <div className="grid grid-cols-3 mt-5">
           <div className="flex flex-col items-start">
-            <Button
-              asChild
-              variant="underline"
-              size="hug"
-              className="font-normal justify-start">
-              <a className="text-h4" href={data?.projectsLink}>
-                {getProjectsCountText(data?.projects || 0)}
-              </a>
-            </Button>
-            <span className="text-muted">реализовано с Astron</span>
+            {!isLoading ? (
+              <>
+                <Button
+                  asChild
+                  variant="underline"
+                  size="hug"
+                  className="font-normal justify-start">
+                  <a className="text-h4" href={data?.projectsLink}>
+                    {getProjectsCountText(data?.projects || 0)}
+                  </a>
+                </Button>
+                <span className="text-muted">реализовано с Astron</span>
+              </>
+            ) : (
+              <>
+                <Skeleton className="w-24 h-4 !rounded-full" />
+                <Skeleton className="w-40 mt-2 h-4 !rounded-full" />
+              </>
+            )}
           </div>
           <div className="flex flex-col items-start">
-            <h4 className="text-h4">
-              {new Intl.NumberFormat("ru-RU", {
-                minimumFractionDigits: 0,
-              }).format(data?.projectsImplementedArea || 0)}
-              &nbsp;м²
-            </h4>
-            <span className="text-muted">реализовано проектов</span>
+            {!isLoading ? (
+              <>
+                <h4 className="text-h4">
+                  {new Intl.NumberFormat("ru-RU", {
+                    minimumFractionDigits: 0,
+                  }).format(data?.projectsImplementedArea || 0)}
+                  &nbsp;м²
+                </h4>
+                <span className="text-muted">реализовано проектов</span>
+              </>
+            ) : (
+              <>
+                <Skeleton className="w-24 h-4 !rounded-full" />
+                <Skeleton className="w-40 mt-2 h-4 !rounded-full" />
+              </>
+            )}
           </div>
           <div className="flex flex-col items-start">
-            <h4 className="text-h4">
-              {getCooperationYearsText(data?.cooperationYears || 0)}
-            </h4>
-            <span className="text-muted">сотрудничества</span>
+            {!isLoading ? (
+              <>
+                <h4 className="text-h4">
+                  {getCooperationYearsText(data?.cooperationYears || 0)}
+                </h4>
+                <span className="text-muted">сотрудничества</span>
+              </>
+            ) : (
+              <>
+                <Skeleton className="w-24 h-4 !rounded-full" />
+                <Skeleton className="w-40 mt-2 h-4 !rounded-full" />
+              </>
+            )}
           </div>
         </div>
         <div className="mt-5">
-          <Button
-            asChild
-            variant="ghost"
-            size="sm"
-            className="font-normal text-primary">
-            <a href={data?.certificate} target="_blank">
-              <Icons.DocumentDownload />
-              <span>Сертификат Партнёра-Строителя</span>
-            </a>
-          </Button>
+          {!isLoading ? (
+            <Button
+              asChild
+              variant="ghost"
+              size="sm"
+              className="font-normal text-primary">
+              <a href={data?.certificate} target="_blank">
+                <Icons.DocumentDownload />
+                <span>Сертификат Партнёра-Строителя</span>
+              </a>
+            </Button>
+          ) : (
+            <Skeleton className="w-72 h-7 !rounded-main" />
+          )}
         </div>
       </div>
     </article>
--- src/entities/news/ui/news-card-skeleton.tsx
+++ src/entities/news/ui/news-card-skeleton.tsx
@@ -4,14 +4,14 @@ export function NewsCardSkeleton() {
   return (
     <div className="bg-card rounded-main py-5 px-7 relative flex gap-4 items-start">
       <div className="flex-1">
-        <Skeleton className="w-28 h-3" />
+        <Skeleton className="w-28 h-3 !rounded-full" />
         <div className="space-y-2 mt-4">
-          <Skeleton className="h-3 w-3/4" />
+          <Skeleton className="h-3 w-3/4 !rounded-full" />
         </div>
         <div className="space-y-2 mt-4">
-          <Skeleton className="h-3 w-3/4" />
-          <Skeleton className="h-3 w-3/5" />
-          <Skeleton className="h-3 w-2/4" />
+          <Skeleton className="h-3 w-3/4 !rounded-full" />
+          <Skeleton className="h-3 w-3/5 !rounded-full" />
+          <Skeleton className="h-3 w-2/4 !rounded-full" />
         </div>
       </div>
       <Skeleton className="shrink-0 !rounded-main w-[9.375rem] h-[7.5rem]" />
