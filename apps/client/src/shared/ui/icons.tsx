204ae32edf62e1439fe5d93979c8b5183191b306
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2025-01-28 03:57:28 -0800

    feat: change loader

--- apps/client/src/shared/ui/icons.tsx
+++ apps/client/src/shared/ui/icons.tsx
@@ -1058,36 +1058,59 @@ export const Icons = {
         height="1em"
         viewBox="0 0 24 24"
         {...props}>
-        <g
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeWidth={2}>
-          <path strokeDasharray={28} strokeDashoffset={28} d="M12 10l4 7h-8Z">
-            <animate
-              fill="freeze"
-              attributeName="stroke-dashoffset"
-              dur="0.4s"
-              values="28;0"></animate>
-          </path>
-          <path d="M12 10l4 7h-8Z" opacity={0}>
-            <animate
-              attributeName="d"
-              begin="0.4s"
-              dur="0.8s"
-              keyTimes="0;0.25;1"
-              repeatCount="indefinite"
-              values="M12 10l4 7h-8Z;M12 4l9.25 16h-18.5Z;M12 4l9.25 16h-18.5Z"></animate>
-            <animate
-              attributeName="opacity"
-              begin="0.4s"
-              dur="0.8s"
-              keyTimes="0;0.1;0.75;1"
-              repeatCount="indefinite"
-              values="0;1;1;0"></animate>
-          </path>
-        </g>
+        <path
+          fill="currentColor"
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+          opacity=".25"
+        />
+        <path
+          fill="currentColor"
+          d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
+          <animateTransform
+            attributeName="transform"
+            dur="0.75s"
+            repeatCount="indefinite"
+            type="rotate"
+            values="0 12 12;360 12 12"
+          />
+        </path>
       </svg>
+      // <svg
+      //   xmlns="http://www.w3.org/2000/svg"
+      //   width="1em"
+      //   height="1em"
+      //   viewBox="0 0 24 24"
+      //   {...props}>
+      //   <g
+      //     fill="none"
+      //     stroke="currentColor"
+      //     strokeLinecap="round"
+      //     strokeWidth={2}>
+      //     <path strokeDasharray={28} strokeDashoffset={28} d="M12 10l4 7h-8Z">
+      //       <animate
+      //         fill="freeze"
+      //         attributeName="stroke-dashoffset"
+      //         dur="0.4s"
+      //         values="28;0"></animate>
+      //     </path>
+      //     <path d="M12 10l4 7h-8Z" opacity={0}>
+      //       <animate
+      //         attributeName="d"
+      //         begin="0.4s"
+      //         dur="0.8s"
+      //         keyTimes="0;0.25;1"
+      //         repeatCount="indefinite"
+      //         values="M12 10l4 7h-8Z;M12 4l9.25 16h-18.5Z;M12 4l9.25 16h-18.5Z"></animate>
+      //       <animate
+      //         attributeName="opacity"
+      //         begin="0.4s"
+      //         dur="0.8s"
+      //         keyTimes="0;0.1;0.75;1"
+      //         repeatCount="indefinite"
+      //         values="0;1;1;0"></animate>
+      //     </path>
+      //   </g>
+      // </svg>
     );
   },
   InfoCircle: (props: React.SVGProps<SVGSVGElement>) => {
