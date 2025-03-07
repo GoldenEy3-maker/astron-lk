4af59f7da27f415ff652228f140ffb2844ae43b2
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2025-03-07 22:04:44 +0700

    feat(footer): add render current year

--- apps/client/src/widgets/footer/ui/footer.tsx
+++ apps/client/src/widgets/footer/ui/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/shared/ui/button";
 export function Footer() {
   return (
     <footer className="col-[main] flex flex-wrap items-center justify-between gap-x-6 gap-y-1 ~py-4/10">
-      <p>© Astron Buildings, 2024 </p>
+      <p>© Astron Buildings, {new Date().getFullYear()} </p>
       <Button asChild variant="link" size="hug" className="text-sm">
         <a href="https://countryagency.ru/" target="_blank">
           сайт от country
